fe869c39b1d5f1e959b39ec6699122f88b854f08
Shrabani Adhikari <shrabaniadhikari03@gmail.com>
2025-07-22 21:29:14 +0530

    Delete PERFECT LOAN directory

--- PERFECT LOAN/js/app.js
+++
@@ -1,7 +0,0 @@
-let toggler=document.querySelector('#toggler');
-let nav_items=document.querySelector('.nav_items');
-toggler.addEventListener('click',listDisplay)
-
-function listDisplay(){
-    nav_items.classList.toggle('showList')
-}
